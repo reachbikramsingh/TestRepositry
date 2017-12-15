9f5631d79626cb9f2f89e23f94f093351e811c73
reachbikramsingh <reachbikramsingh@gmail.com>
2017-12-15 14:33:21 +0530

    insert test

--- welcome/src/test/SaveEmp.java
+++ welcome/src/test/SaveEmp.java
@@ -1,5 +1,13 @@
 package test;
 
+import bean.Employee;
+import dao.EmployeeDao;
+
 public class SaveEmp {
+	public static void main(String[] args) {
+	EmployeeDao dao=new EmployeeDao();	
+	Employee emp_bean =new Employee("bikram","hyderabad",24);
+	dao.insert(emp_bean);
+	}
 
 }
